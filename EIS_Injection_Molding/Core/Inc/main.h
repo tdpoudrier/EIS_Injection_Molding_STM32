7387d71844e581074e71370f4e524503e75dfb47
Tevin Poudrier <tdpoudrier@gmail.com>
2024-04-11 19:51:34 -0600

    Created Application Project and finalized pinout

---
+++ EIS_Injection_Molding/Core/Inc/main.h
@@ -0,0 +1,86 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define User_Button_Pin GPIO_PIN_13
+#define User_Button_GPIO_Port GPIOC
+#define User_Button_EXTI_IRQn EXTI4_15_IRQn
+#define Led_Pin GPIO_PIN_5
+#define Led_GPIO_Port GPIOA
+#define SOLENOID_1_Pin GPIO_PIN_6
+#define SOLENOID_1_GPIO_Port GPIOA
+#define SOLENOID_2_Pin GPIO_PIN_7
+#define SOLENOID_2_GPIO_Port GPIOA
+#define ENCODER_SW_Pin GPIO_PIN_9
+#define ENCODER_SW_GPIO_Port GPIOA
+#define SPI_CS_2_Pin GPIO_PIN_15
+#define SPI_CS_2_GPIO_Port GPIOA
+#define SPI_CS_1_Pin GPIO_PIN_5
+#define SPI_CS_1_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ EIS_Injection_Molding/Core/Src/encoder.c
@@ -0,0 +1,53 @@
+/*
+ * encoder.c
+ *
+ *  Created on: Apr 9, 2024
+ *      Author: Tevin Poudrier
+ */
+
+#include "encoder.h"
+#include "main.h"
+
+void ENC_Init (ENC_Handle* encoder, TIM_HandleTypeDef* timer, GPIO_TypeDef* port, uint16_t pin) {
+	encoder->htim = timer->Instance;
+	encoder->port = port;
+	encoder->pin = pin;
+
+	encoder->prevState = 0;
+	encoder->prevCount = 0;
+
+	HAL_TIM_Encoder_Start(timer, TIM_CHANNEL_ALL);
+}
+
+/**
+ * Only check once per super loop execution
+ */
+uint8_t ENC_ReadSwitch (ENC_Handle* encoder) {
+	GPIO_PinState state = HAL_GPIO_ReadPin (encoder->port, encoder->pin);
+
+	if (state != encoder->prevState) {
+		encoder->prevState = state;
+		return state;
+	}
+	else {
+		return 0;
+	}
+}
+
+uint8_t ENC_GetDirection (ENC_Handle* encoder) {
+	return (encoder->htim->CR1 & 0x0010) >> 4;
+}
+
+uint32_t ENC_GetCount (ENC_Handle* encoder) {
+	return encoder->htim->CNT;
+	//uint32_t count = encoder->htim->CNT;
+//
+//	 if (count - encoder->prevCount > 3 || encoder->prevCount - count > 3) {
+//		 encoder->prevCount = count;
+//		 return count;
+//	 }
+//	 else {
+//		 return encoder->prevCount;
+//	 }
+}
+
